bd5d1c90842c4ed07a63b7b8ba6e20207cefebe9
TanavGupta-Dev <git@tanavgupta.dev>
2025-07-31 22:01:05 +0530

    [main]: Remove .exe files, correct remove_duplicates functionality

--- Arrays/Leetcode/0026_remove_duplicates.cpp
+++ Arrays/Leetcode/0026_remove_duplicates.cpp
@@ -18,30 +18,25 @@ using namespace std;
 
 class Solution {
    public:
-
     /**
      * Given a sorted array of integers (may contain duplicates), return the
-     * number of unique elements & shift all duplicates to the end, keeping 
+     * number of unique elements & shift all duplicates to the end, keeping
      * the relative order of elements the same.
-     * 
+     *
      * Ex. {1, 2, 2, 3, 3}
      * Ans: {1, 2, 3, 2, 3}, 3
      * Explanation: 3 unique elements, duplicates are shifted to the end.
      */
     int removeDuplicates(vector<int>& nums) {
-
         // The first element counts as unique.
         int num_unique_elements = 1;
 
         // Start from the second element
         for (int i = 1; i < nums.size(); i++) {
-
-            // Every time we encounter a unique element (by comparing to previous)
-            // `i - num_unique_elements` would give us the index of oldest pending duplicate.
-            // Replace that with the latest unique element.
+            // For every nth unique element we encounter (by comparing to previous)
+            // `num_unique_elements++` would give us the index of where to place that unique number.
             if (nums[i] != nums[i - 1]) {
-                nums[i - num_unique_elements] = nums[i];
-                num_unique_elements++;
+                nums[num_unique_elements++] = nums[i];
             }
         }
 
@@ -50,12 +45,12 @@ class Solution {
 };
 
 int main() {
-    vector<int> nums = {1, 1, 2};
+    vector<int> nums = {1, 2, 2, 3, 3};
 
-    // prints: 2
-    // nums = {1, 2, 2}
+    // prints: 3
+    // nums = {1, 2, 3, 3, 3}
     cout << Solution().removeDuplicates(nums) << endl;
-    for (auto c: nums) {
+    for (auto c : nums) {
         cout << c << ' ';
     }
 }
